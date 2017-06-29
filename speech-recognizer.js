eb8804489e655cd28233218b1e8120bc453c3a46
kkkksang <kkkksang@naver.com>
2017-06-29 19:03:07 +0900

    Update speech-recognizer.js

--- speech-recognizer.js
+++ speech-recognizer.js
@@ -46,7 +46,8 @@
         };
         recognition.onend = function() {
             $('.speech-content-mic').removeClass('speech-mic-works').addClass('speech-mic');
-        recognition.start();
+        // ★ below for continuous recognition     
+        recognition.start();    
         };
         
     });
