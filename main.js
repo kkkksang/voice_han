3e170881df0a80add4f49a64b380690c39dec439
kkkksang <kkkksang@naver.com>
2017-07-07 18:51:53 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var result = document.getElementById ('result');
+var final_span = document.getElementById ('final_span');
+var intrim_span =document.getElementById ('intrim_span);
+
+var recognition =new webkitSpeechRecognition();
+recognition.continuous = true ;
+recognition.interimResult = true; 
+recognition.onresult = function(event) {
+if(result.isFinal) {
+	var last_spoken = result{0}.transcript.trim();
+	result.innerText = last_spoken;
+	}
+};
+
+recognition.lang = ko-KR;
+recognition.start();
